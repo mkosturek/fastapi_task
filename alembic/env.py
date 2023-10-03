0f007f108abd3860e68086ee4d76ba810f19e68f
Michał Kosturek <michal.kosturek@infermedica.com>
2023-10-03 12:01:48 +0200

    fix alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,13 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.config import settings
-from app.models import Base
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}")
+    "sqlalchemy.url",
+    f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -68,9 +70,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
